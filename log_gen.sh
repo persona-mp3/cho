1d1004b16e602b8465bdfafa5cd0a2f8444bbd70
persona-mp3 <randomnobscurebs@gmail.com>
2026-06-20 19:25:55 +0100

    removed log directory

--- log_gen.sh
+++ log_gen.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 set -e
 
+mkdir -p logs
+
 info_log="INFO  generating random logs. Reason: nostre dama"
 error_log="ERROR could not find #viu08we9aav.log file. Please check the volumes are mounted properly"
 date_fmt=$(date '+%d-%m-%Y %H:%M:%S')
