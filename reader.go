85b0526ab3cf5bd14a6bab4315f1b9f8079247e6
persona-mp3 <randomnobscurebs@gmail.com>
2026-06-20 20:34:45 +0100

    added docs and removed commented code

--- reader.go
+++ reader.go
@@ -1,53 +1,13 @@
 package main
 
 import (
-	// "errors"
-	// "io"
-	// "os"
-	"log"
+	"errors"
+	"io"
 )
 
-// func main() {
-// 	f, err := os.OpenFile("./main.go", os.O_RDWR, 600)
-// 	if err != nil {
-// 		log.Fatal(err)
-// 	}
-//
-// 	info, _ := f.Stat()
-// 	fsize := info.Size()
-// 	pos, err := f.Seek(fsize, 0)
-// 	if err != nil {
-// 		log.Fatal(err)
-// 	}
-//
-// 	log.Println("EOF->", pos)
-// 	n, err := f.WriteAt([]byte("\n\nwhat about confidenitiality? You have ...?\n"), pos)
-// 	if err != nil {
-// 		log.Fatal(err)
-// 	}
-//
-// 	if err := f.Sync(); err != nil {
-// 		log.Fatal(err)
-// 	}
-// 	new_pos := pos + (int64(n))
-// 	log.Println("newPos: ", new_pos)
-//
-// 	log.Println("bytes written: ", n)
-// 	buff := make([]byte, n)
-//
-// 	// when we write to a file i assum seek automatically moves the
-// 	// pointer to the last byte? so where does read start from if we're truly
-// 	// reading from the OG position of the file before writing?
-// 	lastWritten, err := f.ReadAt(buff, pos)
-//
-// 	if err != nil && !errors.Is(err, io.EOF) {
-// 		log.Fatal("read error: ", err, lastWritten, string(buff))
-// 	}
-//
-// 	log.Println("lastWritten:: ", lastWritten)
-// 	log.Println("content: ", string(buff))
-// }
-
+// readLastLog reads the latest appended log by evaluating the number of bytes written
+// It returns the newFileSize, the contents written and an error if [Stat] call failed, or
+// [ReadAt] failed
 func (c *Cho) readLastLog(originalFileSize int64) (int64, []byte, error) {
 	fileInfo, err := c.source.Stat()
 	if err != nil {
@@ -58,18 +18,12 @@ func (c *Cho) readLastLog(originalFileSize int64) (int64, []byte, error) {
 
 	bytesWritten := newFileSize - originalFileSize
 
-	// currPosition, err := c.source.Seek(newFileSize, 0)
-	// if err != nil {
-	// 	return 0, nil, err
-	// }
-
 	buffer := make([]byte, bytesWritten)
 	n, err := c.source.ReadAt(buffer, originalFileSize)
-	if err != nil {
+	_ = n
+	if err != nil && !errors.Is(err, io.EOF){
 		return 0, nil, err
 	}
-
-	log.Println("number of bytesRead: ", n)
 
 	return newFileSize, buffer, nil
 }
